6ab020323c569bf46219580ae6eb6b4a09887137
Peter Ruan <pruan@redhat.com>
2016-01-19 14:18:13 -0800

    fixed typo in license term

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author = "Peter Ruan",
     author_email = "pruan@redhat.com",
     description = "This is a python interface for the new Openshift REST API.",
-    license = "Aache v2",
+    license = "Apache v2",
     keywords = "PaaS Redhat OpenShift",
     url = "https://github.com/openshift/python-interface",   # project home page, if any
 
